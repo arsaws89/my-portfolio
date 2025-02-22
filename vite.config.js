78574eb4472902ca39da5b97828c652319887485
Spring Studios <springstudios@Springs-MacBook-Pro.local>
2025-02-22 12:01:58 +0300

    Update repository URL and base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio-backup/',
+  base: '/my-portfolio/',
   preview: {
     port: 3000,
     strictPort: true,
