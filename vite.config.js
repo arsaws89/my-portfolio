b0047cad6b61d1ef6b5e446caa9cd7fcbcec9d2f
Spring Studios <springstudios@Springs-MacBook-Pro.local>
2025-02-22 18:20:22 +0300

    Update Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/',
-  preview: {
-    port: 3000,
-    strictPort: true,
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
   },
   server: {
-    host: '0.0.0.0',
     port: 5173,
-    hmr: {
-      clientPort: 5173,
-      host: '0.0.0.0'
-    }
+    open: true
   }
 })
